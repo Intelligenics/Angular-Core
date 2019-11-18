2bc40fe845a48d6efe40a0ada100ba9bb464c75f
matthewparton <matthewparton@intelligenics.co.uk>
2019-11-18 14:11:09 +0000

    Updates and some error fixes

--- Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/framework.routing.js
+++ Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/framework.routing.js
@@ -1,52 +1,87 @@
 "use strict";
-var __spreadArrays = (this && this.__spreadArrays) || function () {
-    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
-    for (var r = Array(s), k = 0, i = 0; i < il; i++)
-        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
-            r[k] = a[j];
-    return r;
-};
+//////////////////////////////////////////////////////////////////////////
+///
+/// Intelligenics Angular Application Framework
+/// Copyright(C) 2019 Intelligenics Ltd.  
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+/// sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+/// subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+/// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// Contact Information
+/// 
+/// Name: Matthew Parton M.Sc.
+/// Feel free to contact me using the following details
 Object.defineProperty(exports, "__esModule", { value: true });
 var rxjs_1 = require("rxjs");
 var about_component_1 = require("./components/about/about.component");
-var framework_component_1 = require("./components/framework/framework.component");
 var dialog_component_1 = require("./components/dialog/dialog.component");
 var sidebar_component_1 = require("./components/sidebar/sidebar.component");
 var snackbar_component_1 = require("./components/snackbar/snackbar.component");
 var authentication_guard_1 = require("./guards/authentication.guard");
-var test_component_1 = require("../../../test-harness/src/app/components/test.component");
 var ApplicationRoutes = /** @class */ (function () {
     function ApplicationRoutes() {
     }
     ApplicationRoutes.prepend = function (routes) {
         var _a;
-        (_a = ApplicationRoutes.AppRoutes[0].children).unshift.apply(_a, routes);
+        (_a = ApplicationRoutes.AppRoutes).unshift.apply(_a, routes);
+        ApplicationRoutes.RoutesChangedEvent.next(routes);
     };
     ApplicationRoutes.append = function (routes) {
         var _a;
-        (_a = ApplicationRoutes.AppRoutes[0].children).push.apply(_a, routes);
+        (_a = ApplicationRoutes.AppRoutes).push.apply(_a, routes);
+        ApplicationRoutes.RoutesChangedEvent.next(routes);
     };
     ApplicationRoutes.RoutesChangedEvent = new rxjs_1.Subject();
     ApplicationRoutes.AppRoutes = [];
     return ApplicationRoutes;
 }());
 exports.ApplicationRoutes = ApplicationRoutes;
-// Primary route
-ApplicationRoutes.AppRoutes =
-    [
-        {
-            path: "",
-            component: framework_component_1.FrameworkComponent,
-            canActivate: [authentication_guard_1.AuthenticationGuard],
-            canActivateChild: [authentication_guard_1.AuthenticationGuard],
-            children: [
-                {
-                    path: "",
-                    component: test_component_1.TestComponent,
-                }
-            ],
-        }
-    ];
+ApplicationRoutes.RoutesChangedEvent.subscribe(function (routes) {
+    if (ApplicationRoutes.AppRoutes.length == 6) // primary route
+     {
+        // Secure all routes
+        ApplicationRoutes.AppRoutes.forEach(function (value) {
+            value.canActivate = [authentication_guard_1.AuthenticationGuard];
+            value.canActivateChild = [authentication_guard_1.AuthenticationGuard];
+        });
+    }
+    //routes
+    //// Add sidebar to main wildcard path
+    //routes
+    //    .filter( function ( value )
+    //    {
+    //        return "**" == value.path &&
+    //            null != value.children &&
+    //            null == value.outlet;
+    //    } )
+    //    .map( function ( value )
+    //    {
+    //        var _a;
+    //        ( _a = value.children ).push.apply( _a, SidebarRoutes );
+    //    } );
+});
+//// Primary route
+//ApplicationRoutes.AppRoutes =
+//    [
+//        {
+//            path: "",
+//            component: FrameworkComponent,
+//            //canActivate: [AuthenticationGuard],
+//            //canActivateChild: [AuthenticationGuard],
+//            children:
+//                [
+//                ],
+//        }
+//    ]
 exports.SnackbarRoutes = [
     {
         path: "",
@@ -54,7 +89,7 @@ exports.SnackbarRoutes = [
         component: snackbar_component_1.SnackbarComponent
     }
 ];
-ApplicationRoutes.append(__spreadArrays(exports.SnackbarRoutes));
+ApplicationRoutes.append(exports.SnackbarRoutes.slice());
 exports.AppDialogRoutes = [
     {
         path: "dialog",
@@ -72,7 +107,7 @@ exports.AppDialogRoutes = [
         ]
     },
 ];
-ApplicationRoutes.append(__spreadArrays(exports.AppDialogRoutes));
+ApplicationRoutes.append(exports.AppDialogRoutes.slice());
 /** Framework dialog routes. */
 var DialogRoutes = /** @class */ (function () {
     function DialogRoutes() {
@@ -114,7 +149,7 @@ exports.AppSidebarRoutes = [
         children: []
     },
 ];
-ApplicationRoutes.append(__spreadArrays(exports.AppSidebarRoutes));
+ApplicationRoutes.append(exports.AppSidebarRoutes.slice());
 /** Framework sidebar routes. */
 var SidebarRoutes = /** @class */ (function () {
     function SidebarRoutes() {
--- Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/test-harness/src/app/components/test.component.ts
+++ Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/test-harness/src/app/components/test.component.ts
@@ -1,21 +1,50 @@
+//////////////////////////////////////////////////////////////////////////
+///
+/// Intelligenics Angular Application Framework
+/// Copyright(C) 2019 Intelligenics Ltd.  
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
+/// sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+/// subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+/// 
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+/// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/// 
+/// Contact Information
+/// 
+/// Name: Matthew Parton M.Sc.
+/// Feel free to contact me using the following details
+
+/// LinkedIn Profile: https://www.linkedin.com/in/intelligenics/
+/// Website: http://www.intelligenics.co.uk
+/// Email: matthewparton@intelligenics.co.uk
+///
+//////////////////////////////////////////////////////////////////////////
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
 
 import { DialogOptions, DialogService, MessageType, SidebarPosition, SidebarService, SnackbarService } from '../../../../module/src/public-api';
+import { TestService } from '../services/test.service';
 
-@Component( {
+@Component({
     selector: 'app-test',
     templateUrl: './test.component.html',
     host: { class: 'app-test' }
-} )
+})
 export class TestComponent implements OnInit
 {
     constructor(
         private readonly router: Router,
         private readonly dialogService: DialogService,
         private readonly snackbarService: SnackbarService,
-        private readonly sidebarService: SidebarService )
+        private readonly sidebarService: SidebarService,
+        private readonly testService: TestService)
     {
 
     }
@@ -27,39 +56,39 @@ export class TestComponent implements OnInit
 
     public onShowSidebarLeft(): void
     {
-        this.sidebarService.show( 'content', SidebarPosition.Left );
+        this.sidebarService.show('content', SidebarPosition.Left);
     }
 
     public onShowSidebarRight(): void
     {
-        this.sidebarService.show( 'content', SidebarPosition.Right );
+        this.sidebarService.show('content', SidebarPosition.Right);
     }
 
     public onShowSidebarBottom(): void
     {
-        this.sidebarService.show( 'content', SidebarPosition.Bottom );
+        this.sidebarService.show('content', SidebarPosition.Bottom);
     }
 
     public onShowSidebarTop(): void
     {
-        this.sidebarService.show( 'content', SidebarPosition.Top );
+        this.sidebarService.show('content', SidebarPosition.Top);
     }
 
     public onShowShackbarInfo(): void
     {
-        this.snackbarService.info( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, \
+        this.snackbarService.info('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, \
                                   consectetur adipiscing elit, sed; do eiusmod; tempor; incididunt; ut; labore; et; dolore; magna; aliqua.; ' );
     }
 
     public onShowShackbarWarning(): void
     {
-        this.snackbarService.warning( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.snackbarService.warning('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
 
     }
 
     public onShowShackbarError(): void
     {
-        this.snackbarService.error( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.snackbarService.error('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
     }
 
     public onShowAbout(): void
@@ -69,68 +98,106 @@ export class TestComponent implements OnInit
         let version = '2.0.1';
         let disclaimer = 'Copyright (C) 2019  Intelligenics\r\n \
         \
-        This program is free software: you can redistribute it and/or modify\
-        it under the terms of the GNU General Public License as published by\
-        the Free Software Foundation, either version 3 of the License, or\
-        (at your option) any later version.\r\n\r\n\
+        Intelligenics Angular Application Framework\
+        Copyright(C) 2019 Intelligenics Ltd.\
         \
-        This program is distributed in the hope that it will be useful,\
-        but WITHOUT ANY WARRANTY; without even the implied warranty of\
-        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\
-        GNU General Public License for more details.\r\n\r\n\
+        Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),\
+        to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,\
+        sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,\
+        subject to the following conditions:\
         \
-        You should have received a copy of the GNU General Public License\
-        along with this program.  If not, see <https://www.gnu.org/licenses/>.';
-        this.dialogService.about( version, disclaimer, productName );
+        The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\
+        \
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, \
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, \
+        DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE \
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.';
+
+        this.dialogService.about(version, disclaimer, productName);
     }
 
     public onShowDialog(): void
     {
-        this.dialogService.show( 'content', 'This is the title text here' );
+        this.dialogService.show('content', 'This is the title text here');
     }
 
     public onShowInfo(): void
     {
-        this.dialogService.info( 'this is a info', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.dialogService.info('this is a info', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
     }
 
     public onShowWarning(): void
     {
-        this.dialogService.warning( 'this is a warning', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.dialogService.warning('this is a warning', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
     }
 
     public onShowError(): void
     {
-        this.dialogService.error('this is a error', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.dialogService.error('this is a error', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
     }
 
     public onShowQuestion(): void
     {
-        this.dialogService.question( 'this is a error', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ' );
+        this.dialogService.question('this is a error', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ');
     }
 
     public onShowConfirmation(): void
     {
-        this.dialogService.alert( 'this is a error', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.ConfirmCancel );
+        this.dialogService.alert('this is a error', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.ConfirmCancel);
     }
 
     public onShowSaveCancel(): void
     {
-        this.dialogService.alert('this is a message', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.SaveCancel );
+        this.dialogService.alert('this is a message', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.SaveCancel);
     }
 
     public onShowYesNo(): void
     {
-        let sub = this.dialogService.dialogClosingEvent.subscribe( ( value ) =>
+        let sub = this.dialogService.dialogClosingEvent.subscribe((value) =>
         {
-            this.snackbarService.info( `you selected ${DialogOptions[value.selectedOption]} ` );
+            this.snackbarService.info(`you selected ${DialogOptions[value.selectedOption]} `);
             sub.unsubscribe();
-        } );
-        this.dialogService.alert( 'this is a message', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.YesNo );
+        });
+        this.dialogService.alert('this is a message', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.YesNo);
     }
 
     public onShowYes(): void
     {
-        this.dialogService.alert( 'this is a message', 'Are you sure you want to do this?', MessageType.Info, DialogOptions.Yes );
+        let sub = this.dialogService.dialogClosingEvent.subscribe((value) =>
+        {
+            sub.unsubscribe();
+        });
+        this.dialogService.show('this is a message', 'Are you sure you want to do this?', false);
+    }
+
+
+    public onTrapDialog()
+    {
+        let sub = this.dialogService.dialogClosingEvent.subscribe((value) =>
+        {
+            sub.unsubscribe();
+        });
+        this.dialogService.show('myroutepath', 'Are you sure you want to do this?', false);
+    }
+
+
+    public onTestAPIError(): void
+    {
+        this.testService.getUnhandledError().subscribe();
+    }
+
+    public onTestLongRunning(): void
+    {
+        this.testService.getLongRunningOperation().subscribe();
+    }
+
+    public onTestValidationError(): void
+    {
+        this.testService.getValidationError().subscribe();
+    }
+
+    public onTestInvalidEndpoint(): void
+    {
+        this.testService.getInvalidEndpoint().subscribe();
     }
 }
