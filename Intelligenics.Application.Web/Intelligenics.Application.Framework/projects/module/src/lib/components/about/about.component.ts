876835bbc74497d52592b40782162b373160ffed
matthewparton <matthewparton@intelligenics.co.uk>
2020-01-28 14:11:31 +0000

    bugfix/resolve-styles-links

--- Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/components/about/about.component.ts
+++ Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/components/about/about.component.ts
@@ -1,22 +1,22 @@
 //////////////////////////////////////////////////////////////////////////
 ///
 /// Intelligenics Angular Application Framework
-/// Copyright(C) 2019 Intelligenics Ltd.  
-/// 
+/// Copyright(C) 2019 Intelligenics Ltd.
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
 /// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
 /// sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 /// subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 /// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/// 
+///
 /// Contact Information
-/// 
+///
 /// Name: Matthew Parton M.Sc.
 /// Feel free to contact me using the following details
 
@@ -25,17 +25,18 @@
 /// Email: matthewparton@intelligenics.co.uk
 ///
 //////////////////////////////////////////////////////////////////////////
-import { Component, OnInit,ViewEncapsulation } from '@angular/core';
+
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component( {
-    selector: "int-app-about",
-    templateUrl: 'about.component.html',
     encapsulation: ViewEncapsulation.None,
-    styleUrls: ['about.component.scss']
+    selector: 'int-app-about',
+    styleUrls: ['about.component.scss'],
+    templateUrl: 'about.component.html'
 } )
-export class AboutComponent implements OnInit
+export class AboutComponent
 {
     /** name of the product */
     public productName: string;
@@ -65,9 +66,9 @@ export class AboutComponent implements OnInit
         } ) );
     }
 
-    public ngOnInit(): void
-    {
-    }
+    /**
+     * closes the window
+     */
     public onCloseClicked(): void
     {
         this.router.navigate( [{ outlets: { dialog: null } }] );
