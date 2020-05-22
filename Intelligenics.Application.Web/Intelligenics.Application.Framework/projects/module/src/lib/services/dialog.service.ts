08f09e5b8baae803b585cd13b2fd947ae5e2d6a5
matthewparton <matthewparton@intelligenics.co.uk>
2020-05-22 12:53:13 +0100

    Remoe unused code fragements

--- Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/services/dialog.service.ts
+++ Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/module/src/lib/services/dialog.service.ts
@@ -33,7 +33,7 @@ import { Subject } from 'rxjs';
 /**
  * The dialog service provide functions to show a modal dialog window
  */
-@Injectable( { providedIn: 'root' } )
+@Injectable({ providedIn: 'root' })
 export class DialogService
 {
 
@@ -47,7 +47,7 @@ export class DialogService
     /**
      * Creates a new dialog service instance.
      */
-    constructor( private readonly router: Router )
+    constructor(private readonly router: Router)
     {
         this.dialogClosingEvent = new Subject<DialogClosingEventArgs>();
         this.dialogClosedEvent = new Subject<any>();
@@ -58,9 +58,9 @@ export class DialogService
      * @param title the title of the message
      * @param message the message to show
      */
-    public warning( title: string, message: string ): void
+    public warning(title: string, message: string): void
     {
-        this.alert( title, message, MessageType.Warning );
+        this.alert(title, message, MessageType.Warning);
     }
 
     /**
@@ -68,9 +68,9 @@ export class DialogService
      * @param title the title of the message
      * @param message the message to show
      */
-    public error( title: string, message: string ): void
+    public error(title: string, message: string): void
     {
-        this.alert( title, message, MessageType.Error );
+        this.alert(title, message, MessageType.Error);
     }
 
     /**
@@ -78,18 +78,18 @@ export class DialogService
      * @param title the title of the message
      * @param message the message to show
      */
-    public question( title: string, message: string ): void
+    public question(title: string, message: string): void
     {
-        this.alert( title, message, MessageType.Question, DialogOptions.YesNo );
+        this.alert(title, message, MessageType.Question, DialogOptions.YesNo);
     }
     /**
      * Shows an informational alert message to the user
      * @param title the title of the message
      * @param message the message to show
      */
-    public info( title: string, message: string ): void
+    public info(title: string, message: string): void
     {
-        this.alert( title, message, MessageType.Info );
+        this.alert(title, message, MessageType.Info);
     }
 
     /**
@@ -99,7 +99,7 @@ export class DialogService
      * @param messageType the type of message
      * @param allowClose indicates if a close button is allowed on the dialog window
      */
-    public alert( title: string, message: string, messageType: MessageType = MessageType.Info, options: DialogOptions = DialogOptions.OK, allowClose: boolean = true ): void
+    public alert(title: string, message: string, messageType: MessageType = MessageType.Info, options: DialogOptions = DialogOptions.OK, allowClose: boolean = true): void
     {
         this.router.navigate(
             [{ outlets: { dialog: 'dialog' } }],
@@ -113,23 +113,23 @@ export class DialogService
                     messageType,
                     allowClose
                 }
-            } );
+            });
     }
 
     /**
      * notification for all subscribers that the dialog is closing
      * @param args allowing option to prevent the closure of the sidebar
      */
-    public notifyDialogClosing( args: DialogClosingEventArgs ): void
+    public notifyDialogClosing(args: DialogClosingEventArgs): void
     {
-        this.dialogClosingEvent.next( args );
+        this.dialogClosingEvent.next(args);
     }
 
     /**
      * Shows the about dialog
      * @param allowClose optional value indicates if the dialog window provides a close button
      */
-    public about( version: string, disclaimer: string, productName: string ): void
+    public about(version: string, disclaimer: string, productName: string): void
     {
         this.router.navigate(
             [{ outlets: { dialog: 'dialog/about' } }],
@@ -143,7 +143,7 @@ export class DialogService
                     productName,
                     allowClose: true
                 }
-            } );
+            });
     }
     /**
      * Shows the dialog with the specified route path to the dialog'.
@@ -151,7 +151,7 @@ export class DialogService
      * @param title the dialog title
      * @param allowClose optional value indicates if the dialog window provides a close button
      */
-    public show( route: string, title: string, allowClose: boolean = true ): void
+    public show(route: string, title: string, allowClose: boolean = true, data: any = null): void
     {
         this.router.navigate(
             [{ outlets: { dialog: `dialog/${route}` } }],
@@ -162,7 +162,7 @@ export class DialogService
                     title,
                     allowClose
                 }
-            } );
+            });
     }
 
     /**
@@ -170,7 +170,7 @@ export class DialogService
      */
     public close(): void
     {
-        this.dialogClosedEvent.next( null );
+        this.dialogClosedEvent.next(null);
     }
 }
 
--- Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/test-harness/src/app/test-harness.routing.ts
+++ Intelligenics.Application.Web/Intelligenics.Application.Framework/projects/test-harness/src/app/test-harness.routing.ts
@@ -1,100 +1,12 @@
-//////////////////////////////////////////////////////////////////////////
-///
-/// Intelligenics Angular Application Framework
-/// Copyright(C) 2019 Intelligenics Ltd.  
-/// 
-/// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-/// to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
-/// sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-/// subject to the following conditions:
-/// 
-/// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-/// 
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-/// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
-/// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-/// 
-/// Contact Information
-/// 
-/// Name: Matthew Parton M.Sc.
-/// Feel free to contact me using the following details
+// Angular
+import { RouterModule } from '@angular/router'; 
+import { ApplicationRoutes as ApplicationFrameworkRoutes } from "@intelligenics/application-framework";
+import { FrameworkRoutes } from '../../../module/src/lib/framework.routing'; 
 
-/// LinkedIn Profile: https://www.linkedin.com/in/intelligenics/
-/// Website: http://www.intelligenics.co.uk
-/// Email: matthewparton@intelligenics.co.uk
-///
-//////////////////////////////////////////////////////////////////////////
 
-import { RouterModule } from '@angular/router';
-import { ApplicationRoutes, DialogRoutes, SidebarRoutes } from '../../../module/src/lib/framework.routing';
-import { SidebarPosition } from '../../../module/src/public-api';
-import { ContentComponent } from './components/content.component';
-import { NavigationComponent } from './components/navigation.component';
-import { TestComponent } from './components/test.component';
+ApplicationFrameworkRoutes.append(  FrameworkRoutes );
 
-ApplicationRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: '',
-            component: TestComponent
-        },
-    ]);
-
-DialogRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: 'content',
-            component: ContentComponent
-        },
-    ]
-);
-
-SidebarRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: 'content',
-            component: NavigationComponent
-        },
-    ],
-    SidebarPosition.Left
-);
-
-SidebarRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: 'content',
-            component: NavigationComponent
-        },
-    ],
-    SidebarPosition.Right
-);
-SidebarRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: 'content',
-            component: ContentComponent
-        },
-    ],
-    SidebarPosition.Top
-);
-SidebarRoutes.append(
-    [
-        // Primary Routes
-        {
-            path: 'content',
-            component: ContentComponent
-        },
-    ],
-    SidebarPosition.Bottom
-);
-
-export const AppRouting = RouterModule.forRoot(ApplicationRoutes.AppRoutes,
+export const AppRouting = RouterModule.forRoot(ApplicationFrameworkRoutes.AppRoutes,
     {
         // enableTracing: true
     });
